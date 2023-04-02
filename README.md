9d679c03f954ff584c20ddc78709504d6b7713c5
Arthur <arthursantos48211@gmail.com>
2023-04-02 14:46:35 -0300

    Toques finais

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1 align="center"> Music-player com JavaScript </h1>
+
+<img src="./imgs/site.png">
+
+<h2 align="center">Um music-player simples feito com Javascript puro</h2>
+
+<h2> ➢ O que é esse projeto? </h2>
+<p> 
+  Este projeto foi feito com o objetivo de treinar novos eventos DOM do Javascript.
+  Como é um treino simples, o código está bem amador, então... Relevem kkk 
+</p>
+<p>
+  Você pode acessar o site clicando neste link: https://quimicafacil.vercel.app
+</p>
+
+<h3> ➢ Outras informações </h3>
+<ul>
+  <li> <strong>Status:</strong> <em>Finalizado</em>
+  <li> <strong>Linguagens utilizadas:</strong> <br>
+    <div style="display: inline-block;">
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" width="40" height="40" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" width="40" height="40" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" width="40" height="40"/>
+    </div>
+  <li> <strong>Conhecimentos aplicados:</strong>
+  <ul>
+    <li> <em>CSS Custom Properties</em>
+    <li> <em>Media Queries</em>
+    <li> <em>JavaScript DOM</em>
+  </ul>
+</ul>
--- js/player.js
+++ js/player.js
@@ -1,11 +1,40 @@
 const audio = document.querySelector('audio');
 const playBtn = document.querySelector('#play');
 const pauseBtn = document.querySelector('#pause');
+const select = document.querySelector('select');
 
-playBtn.addEventListener('click', () => {
+const imgMusic = document.querySelector('img');
+
+playBtn.addEventListener('click', function () {
   audio.play();
+  imgMusic.classList.add('play');
 });
 
-pauseBtn.addEventListener('click', () => {
+pauseBtn.addEventListener('click', function () {
   audio.pause();
+  imgMusic.classList.remove('play');
+});
+
+select.addEventListener('change', function () {
+  const musicas = {
+    './audios/Dri - FIM DO MUNDO .mp3': 'Dri - Fim do Mundo',
+    './audios/Billie Jean.mp3': 'Michael Jackson - Billie Jean',
+    './audios/Manoel Gomes - Caneta Azul.mp3': 'Manoel Gomes - Caneta Azul'
+  };
+  const thumbs = {
+    './audios/Dri - FIM DO MUNDO .mp3': './imgs/musica1-capa.jpg',
+    './audios/Billie Jean.mp3': './imgs/musica2-capa.jpg',
+    './audios/Manoel Gomes - Caneta Azul.mp3': './imgs/musica3-capa.jpeg'
+  }
+
+  const musicName = document.querySelector('div#music-name');
+
+  const optChecked = document.querySelector('option:checked').value;
+
+  audio.src = optChecked;
+  musicName.innerHTML = '<p>' + musicas[optChecked] + '</p>';
+  imgMusic.src = thumbs[optChecked];
+
+  audio.play();
+  imgMusic.classList.add('play');
 });
